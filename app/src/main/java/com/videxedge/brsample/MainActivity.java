2ae6ccde25462c3f27c8399ddd6ff2a890820e27
albertschool2015 <albert.school2015@gmail.com>
2019-04-02 09:46:51 +0300

    End of sample

--- app/src/main/java/com/videxedge/brsample/MainActivity.java
+++ app/src/main/java/com/videxedge/brsample/MainActivity.java
@@ -1,13 +1,33 @@
 package com.videxedge.brsample;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    BroadcastBatt broadcastBatt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        broadcastBatt = new BroadcastBatt();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        registerReceiver(broadcastBatt, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        unregisterReceiver(broadcastBatt);
     }
 }
